946d3bc371dac7ec083127f62b115181ba0edf6c
Kiran Anto Sebastian <51168158+Antosebastian@users.noreply.github.com>
2020-07-17 00:10:19 -0400

    Add files via upload

--- forecast/test.py
+++ forecast/test.py
@@ -1,11 +1,11 @@
-import requests, json 
+import requests, json
 response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=toronto&appid=bf6bba48092edcf1ad4e563fe5b6e4e7") 
 x = response.json() 
 if x["cod"] != "404":
 	y = x["main"] 
 	current_temperature = y["temp"] 
 	current_pressure = y["pressure"]
-	current_humidiy = y["humidity"]
+	current_humidity = y["humidity"]
 	z = x["weather"]
 	weather_description = z[0]["description"]
 	print(" Temperature (in kelvin unit) = " +
@@ -13,13 +13,29 @@ if x["cod"] != "404":
           "\n atmospheric pressure (in hPa unit) = " +
                     str(current_pressure) +
           "\n humidity (in percentage) = " +
-                    str(current_humidiy) +
+                    str(current_humidity) +
           "\n description = " +
                     str(weather_description))
 else:
 	print(" City Not Found ")
 
-#path = '/home/Desktop/weather.txt'
+#path = '/home/
+
+#import requests, json 
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=bf6bba48092edcf1ad4e563fe5b6e4e7") 
+x = response.json()
+if x["cod"] != "404":
+	y = x["list"] 
+	za = y[0]["weather"]
+	zb = y[1]["weather"]
+	weather_description_zero = za[0]["description"]
+	weather_description_one = zb[0]["description"]
+
 f= open("weather.txt","w+")
-f.write(weather_description)
+f.write("temperature : " + '{} \n'.format(current_temperature) + "\n")
+f.write("Pressure : " + '{} \n'.format(current_pressure) + "\n")
+f.write("humidity : " + '{} \n'.format(current_humidity) + "\n")
+f.write("current weather : " + weather_description + "\n")
+f.write("weather in next 3 hrs : " + weather_description_zero + "\n")
+f.write("weather in next 6 hrs :" + weather_description_one + "\n")
 f.close
