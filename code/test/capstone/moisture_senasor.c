165f85c8d52095f5203b29697e448637a2ad1b85
Kiran Anto Sebastian <51168158+Antosebastian@users.noreply.github.com>
2020-07-24 23:02:09 -0400

    Update moisture_senasor.c

--- code/test/capstone/moisture_senasor.c
+++ code/test/capstone/moisture_senasor.c
@@ -1,4 +1,21 @@
-char* moisture_sensor()
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void main()
 {
-    return "Moisture sensor not confgured/linked";
+        FILE *fp;
+        int value;
+        while(1)
+        {
+                fp = fopen("/sys/bus/iio/devices/iio:device0/in_voltage4_raw","r");
+                fscanf(fp, "%d", &value);
+//              printf("%d \n", value);
+                if (value <= 451)
+                        printf("wet\n");
+                else
+                        printf("dry\n");
+                fclose(fp);
+                sleep(1);
+        }
 }
